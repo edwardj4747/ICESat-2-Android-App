47761606410bea83d80fdeecdfd18709370deae3
Edward <edward.jahoda@gmail.com>
2020-05-29 15:01:09 -0400

    check validity of lat, long, and radius inputs

--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/MainActivity.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/MainActivity.kt
@@ -1,14 +1,19 @@
 package gov.nasa.gsfc.icesat2.icesat_2
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import gov.nasa.gsfc.icesat2.icesat_2.ui.search.ISearchFragmentCallback
 import gov.nasa.gsfc.icesat2.icesat_2.ui.search.SearchFragment
 import kotlinx.android.synthetic.main.fragment_search.*
 
 
 private const val TAG = "MainActivity"
+private const val LAT_INPUT_ERROR = "Please Enter Latitude between -86.0 and 86.0 ${0x00B0.toChar()}N"
+private const val LONG_INPUT_ERROR = "Please Enter Longitude between -180.0 and 180.0 ${0x00B0.toChar()}E"
+private const val RADIUS_INPUT_ERROR = "Please Enter Radius between 1.1 and 25.0"
 
 class MainActivity : AppCompatActivity(), ISearchFragmentCallback {
 
@@ -42,44 +47,65 @@ class MainActivity : AppCompatActivity(), ISearchFragmentCallback {
     override fun searchButtonPressed() {
         Log.d(TAG, "MainActivity: Search Button Pressed")
 
-        //lat range -86, 86; lon range -180 180
-        if (editTextLat.text.toString() != "") {
-            val lat: Double = (editTextLat.text.toString()).toDouble()
-            if (lat < - 86 || lat > 86) {
-                Log.d(TAG, "invalid lat")
-                return
-            }
-        } else {
-            Log.d(TAG, "Please Enter Latitude")
-            return
+
+        val inputs = allInputsValid() //returns array of {lat, long, radius} if valid. null if not valid
+        if (inputs != null) {
+            //http://icesat2app-env.eba-gvaphfjp.us-east-1.elasticbeanstalk.com/find?lat=-38.9&lon=78.1&r=25&u=miles
+            val serverLocation = "http://icesat2app-env.eba-gvaphfjp.us-east-1.elasticbeanstalk.com/find?lat=${inputs[0]}&lon=${inputs[1]}&r=${inputs[2]}&u=miles"
+            Log.d(TAG, "starting download from $serverLocation")
+            val downloadData = DownloadData()
+            downloadData.startDownload(serverLocation)
         }
 
-        if (editTextLon.text.toString() != "") {
-            val long: Double = (editTextLon.text.toString()).toDouble()
-            if (long < -180 || long > 180) {
-                Log.d(TAG, "invalid Long")
-                return
-            }
-        } else {
-            Log.d(TAG, "Please Enter Longitude")
-            return
-        }
-
-        if (editTextRadius.text.toString() != "") {
-            val radius = editTextRadius.text.toString().toDouble()
-            if (radius > 25) {
-                Log.d(TAG, "invalid radius")
-                return
-            }
-        } else {
-            Log.d(TAG, "Please Enter Radius")
-            return
-        }
-
-
-
-        val downloadData = DownloadData()
-        downloadData.startDownload("http://icesat2app-env.eba-gvaphfjp.us-east-1.elasticbeanstalk.com/find?lat=-38.9&lon=78.1&r=25&u=miles")
         Log.d(TAG, "MainActivity: SearchButtonPressed ends")
+    }
+
+    private fun createSnackBar(text: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Snackbar.make(findViewById(R.id.container), text, Snackbar.LENGTH_LONG)
+                .setAction("OK") {  }
+                .setBackgroundTint(resources.getColor(R.color.colorPrimary, null))
+                .show()
+        } else {
+            Snackbar.make(findViewById(R.id.container), text, Snackbar.LENGTH_LONG)
+                .setAction("OK") {  }
+                .setBackgroundTint(resources.getColor(R.color.colorPrimary))
+                .show()
+        }
+    }
+
+    //return null if there is an error with one of the inputs. Otherwise return array of (lat, lng, radius
+    private fun allInputsValid(): DoubleArray? {
+        //lat range -86, 86; lon range -180 180
+        if (editTextLat.text.toString() == "") {
+            createSnackBar(LAT_INPUT_ERROR)
+            return null
+        }
+        val lat = editTextLat.text.toString().toDouble()
+        if (lat < - 86 || lat > 86) {
+            createSnackBar(LAT_INPUT_ERROR)
+            return null
+        }
+
+        if (editTextLon.text.toString() == "") {
+            createSnackBar(LONG_INPUT_ERROR)
+            return null
+        }
+        val long = editTextLon.text.toString().toDouble()
+        if (long < -180 || long > 180) {
+            createSnackBar(LONG_INPUT_ERROR)
+            return null
+        }
+
+        if (editTextRadius.text.toString() == "") {
+            createSnackBar(RADIUS_INPUT_ERROR)
+            return null
+        }
+        val radius = editTextRadius.text.toString().toDouble()
+        if (radius < 1.1 || radius > 25) {
+            createSnackBar(RADIUS_INPUT_ERROR)
+            return null
+        }
+        return doubleArrayOf(lat, long, radius)
     }
 }
