fde5e4ac0cdaa6c4e3703dbe29831a90f8fb3aa9
Edward <edward.jahoda@gmail.com>
2020-06-03 14:47:45 -0400

    bottom navigation working (fixed menu ids to match navigation ids

--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/MainActivity.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/MainActivity.kt
@@ -49,9 +49,10 @@ class MainActivity : AppCompatActivity(), ISearchFragmentCallback {
         //toolbar.setupWithNavController(navController, appBarConfiguration)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_home, R.id.navigation_favorites, R.id.navigation_info))
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_search, R.id.navigation_favorites, R.id.navigation_info))
 
         setupActionBarWithNavController(navController, appBarConfiguration)
+        bottom_nav_view.setupWithNavController(navController)
 
         mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
