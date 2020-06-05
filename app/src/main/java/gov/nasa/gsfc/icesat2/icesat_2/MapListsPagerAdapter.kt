e3053345328ef5cd3cef142bc89b920b94754c96
Edward <edward.jahoda@gmail.com>
2020-06-04 21:01:12 -0400

    created MapList tabs using viewPager

---
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/MapListsPagerAdapter.kt
@@ -0,0 +1,40 @@
+package gov.nasa.gsfc.icesat2.icesat_2
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+
+private val TAB_TITLES = arrayOf(
+    R.string.map,
+    R.string.list
+)
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+class MapListsPagerAdapter(private val context: Context, fm: FragmentManager)
+    : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return if (position == 0) {
+            MapFragment()
+        } else {
+            ListFragment()
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+
+    override fun getCount(): Int {
+        // Show 2 total pages.
+        return 2
+    }
+}
--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ResultsHolderFragment.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ResultsHolderFragment.kt
@@ -1,18 +1,11 @@
 package gov.nasa.gsfc.icesat2.icesat_2
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_results_holder.*
-import java.lang.Exception
 
 /**
  * A simple [Fragment] subclass.
@@ -21,53 +14,18 @@ private const val TAG = "ResultsHolderFragment"
 
 class ResultsHolderFragment : Fragment() {
 
-    private var tabClickedCount = 0
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_results_holder, container, false)
-
-        /*val backButtonListener = object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                val overallNavController = findNavController()
-                Log.d(TAG, "back button pressed")
-            }
-        }
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, backButtonListener)*/
-
-        return view
+        return inflater.inflate(R.layout.fragment_results_holder, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val navController = activity?.findNavController(R.id.results_holder_host)
-
-
-        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-
-            }
-
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                tabClickedCount++
-                when (tab?.position) {
-                    0 -> {
-                        navController?.navigate(R.id.action_list_fragment_to_map_fragment)
-                        //navController?.popBackStack(R.id.action_list_fragment_to_map_fragment, true)
-                    }
-                    1 -> {
-                        navController?.navigate(R.id.action_mapFragment_to_listFragment)
-
-                    }
-                }
-            }
-        })
+        val sectionsPagerAdapter = MapListsPagerAdapter(requireContext(), childFragmentManager)
+        viewPager.adapter = sectionsPagerAdapter
+        tabs.setupWithViewPager(viewPager)
     }
 
 }
