964b1b9648e8ee05c03b2a7a328fa4067fee8f8f
Edward <edward.jahoda@gmail.com>
2020-06-05 12:31:08 -0400

    text info with links

--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ui/info/InfoFragment.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ui/info/InfoFragment.kt
@@ -1,14 +1,13 @@
 package gov.nasa.gsfc.icesat2.icesat_2.ui.info
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import gov.nasa.gsfc.icesat2.icesat_2.R
+import kotlinx.android.synthetic.main.fragment_info.*
 
 class InfoFragment : Fragment() {
 
@@ -19,13 +18,13 @@ class InfoFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(InfoViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_info, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        /*notificationsViewModel =
+                ViewModelProviders.of(this).get(InfoViewModel::class.java)*/
+        return inflater.inflate(R.layout.fragment_info, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        textViewInfo.movementMethod = LinkMovementMethod.getInstance()
     }
 }
