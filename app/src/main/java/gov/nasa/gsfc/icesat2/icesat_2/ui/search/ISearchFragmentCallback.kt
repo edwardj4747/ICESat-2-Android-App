6d26160a17e2ccad74ba5ba778b5e938dd690533
Edward <edward.jahoda@gmail.com>
2020-06-05 23:12:35 -0400

    modified interface to reflect serverLocation being constructed in MainActivity

--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ui/search/ISearchFragmentCallback.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ui/search/ISearchFragmentCallback.kt
@@ -1,7 +1,7 @@
 package gov.nasa.gsfc.icesat2.icesat_2.ui.search
 
 interface ISearchFragmentCallback {
-    fun searchButtonPressed(serverLocation: String, lat: Double, long: Double, radius: Double)
+    fun searchButtonPressed(lat: Double, long: Double, radius: Double)
 
     fun useCurrentLocationButtonPressed()
 
