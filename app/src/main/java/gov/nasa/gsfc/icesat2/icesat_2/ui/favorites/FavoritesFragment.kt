3d30d600f971283bb9882c6a721ca97e3a1737a1
Edward <edward.jahoda@gmail.com>
2020-06-08 14:37:52 -0400

    Delete All Favorites menu icon

--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ui/favorites/FavoritesFragment.kt
@@ -1,10 +1,9 @@
 package gov.nasa.gsfc.icesat2.icesat_2.ui.favorites
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -36,6 +35,7 @@ class FavoritesFragment : Fragment() {
             initializeRecyclerView()
         })
 
+        setHasOptionsMenu(true)
         return root
     }
 
@@ -44,4 +44,29 @@ class FavoritesFragment : Fragment() {
       favoriteRecyclerView.adapter = adapter
        favoriteRecyclerView.layoutManager = LinearLayoutManager(requireContext())
    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menuDelete -> {
+                showDialog(getString(R.string.deleteAllTitle), getString(R.string.deleteAllDescription), getString(R.string.yes), getString(R.string.cancel))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showDialog(title: String, description: String, positive: String, negative: String) {
+        val alertBuilder = AlertDialog.Builder(requireContext())
+        alertBuilder.setMessage(description)
+            ?.setTitle(title)
+            ?.setPositiveButton(positive) { dialog, which ->
+                favoritesViewModel.deleteAll()
+            }
+            ?.setNegativeButton(negative) {dialog, which ->  }
+        alertBuilder.show()
+    }
 }
