b87436d62ab31ce6d8863b027c2c4ebedd4e3de8
Edward <edward.jahoda@gmail.com>
2020-06-11 09:55:40 -0400

    Gallery Display Fragment created and passing arguments

--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ui/gallery/GalleryDisplay.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ui/gallery/GalleryDisplay.kt
@@ -1,31 +1,30 @@
 package gov.nasa.gsfc.icesat2.icesat_2.ui.gallery
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import gov.nasa.gsfc.icesat2.icesat_2.R
-import kotlinx.android.synthetic.main.fragment_gallery.*
+import kotlinx.android.synthetic.main.fragment_gallery_display.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-private const val TAG = "GalleryFragment"
-
 /**
  * A simple [Fragment] subclass.
- * Use the [GalleryFragment.newInstance] factory method to
+ * Use the [GalleryDisplay.newInstance] factory method to
  * create an instance of this fragment.
  */
-class GalleryFragment : Fragment(), View.OnClickListener {
+class GalleryDisplay : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private val args: GalleryDisplayArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,17 +39,14 @@ class GalleryFragment : Fragment(), View.OnClickListener {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false)
+        return inflater.inflate(R.layout.fragment_gallery_display, container, false)
     }
-
-    //TODO: resize the image to have multiple version
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        imageView1.setOnClickListener(this)
-        imageView2.setOnClickListener(this)
-        imageView3.setOnClickListener(this)
-        imageView4.setOnClickListener(this)
+        textViewTitle.text = args.title
+        imageViewDisplay.setImageResource(args.image)
+        textViewDescription.text = args.description
     }
 
     companion object {
@@ -60,36 +56,16 @@ class GalleryFragment : Fragment(), View.OnClickListener {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment GalleryFragment.
+         * @return A new instance of fragment GalleryDisplay.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            GalleryFragment().apply {
+            GalleryDisplay().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.imageView1 -> {
-                Log.d(TAG, "imageView one clicked")
-                val params = GalleryFragmentDirections.actionNavigationGalleryToGalleryDisplay("Title 1", R.drawable.icesatc, "Description 1")
-                findNavController().navigate(params)
-
-            }
-            R.id.imageView2 -> {
-
-            }
-            R.id.imageView3 -> {
-
-            }
-            R.id.imageView4 -> {
-
-            }
-        }
     }
 }
