a711a3b5429e81a3ae49a5cc29b18db5f34a369d
Edward <edward.jahoda@gmail.com>
2020-06-12 10:05:52 -0400

    code cleanup by removing factory methods

--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ui/gallery/GalleryContainerFragment.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ui/gallery/GalleryContainerFragment.kt
@@ -11,11 +11,6 @@ import androidx.navigation.fragment.navArgs
 import gov.nasa.gsfc.icesat2.icesat_2.R
 import kotlinx.android.synthetic.main.fragment_gallery_container.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 
 class GalleryPagerAdapter(fm: FragmentManager)
     : FragmentStatePagerAdapter(fm) {
@@ -32,25 +27,13 @@ class GalleryPagerAdapter(fm: FragmentManager)
 
 
 /**
- * A simple [Fragment] subclass.
- * Use the [GalleryContainerFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * Recive the index passed in from [GalleryFragment] through args variable and use that to tell the
+ * viewPager which index to display
  */
 class GalleryContainerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private val args by navArgs<GalleryContainerFragmentArgs>()
     private var index = 0
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -70,23 +53,4 @@ class GalleryContainerFragment : Fragment() {
         galleryViewPager.setCurrentItem(index, true)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GalleryContainerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GalleryContainerFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
