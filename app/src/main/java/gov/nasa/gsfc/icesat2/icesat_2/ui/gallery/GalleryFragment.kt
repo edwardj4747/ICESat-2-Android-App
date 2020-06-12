a1bc8066e68f03dd36ade5e787262f9e937e460b
Edward <edward.jahoda@gmail.com>
2020-06-12 10:03:01 -0400

    code cleanup (removed unused factory methods)

--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ui/gallery/GalleryFragment.kt
@@ -9,30 +9,13 @@ import androidx.navigation.fragment.findNavController
 import gov.nasa.gsfc.icesat2.icesat_2.R
 import kotlinx.android.synthetic.main.fragment_gallery.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 private const val TAG = "GalleryFragment"
 
 /**
- * A simple [Fragment] subclass.
- * Use the [GalleryFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * Displays a grid of images set in the [fragment_gallery] layout file and assign on on-click
+ * listener to each button to start the galleryContainer fragment with the specified parameter
  */
 class GalleryFragment : Fragment(), View.OnClickListener {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -64,26 +47,6 @@ class GalleryFragment : Fragment(), View.OnClickListener {
         imageView16.setOnClickListener(this)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GalleryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GalleryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-
     override fun onClick(v: View?) {
         var index = 0
         when (v?.id) {
@@ -104,11 +67,6 @@ class GalleryFragment : Fragment(), View.OnClickListener {
             R.id.imageView15 -> index = 14
             R.id.imageView16 -> index = 15
         }
-        //val params = GalleryFragmentDirections.actionNavigationGalleryToGalleryDisplay(titles[index], images[index], descriptions[index], index)
-
-
-        /*val params = GalleryFragmentDirections.actionNavigationGalleryToGalleryDisplay(index)
-        findNavController().navigate(params)*/
 
         val params = GalleryFragmentDirections.actionNavigationGalleryToGalleryContainerFragment2(index)
         findNavController().navigate(params)
