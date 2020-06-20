e79c0df6de41f2166eaffdeb41165164b1cc81ea
Edward <edward.jahoda@gmail.com>
2020-06-20 17:47:10 -0400

    favorites recyclerView onClick Listener

--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/FavoritesAdapter.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/FavoritesAdapter.kt
@@ -2,9 +2,11 @@ package gov.nasa.gsfc.icesat2.icesat_2
 
 import android.content.Context
 import android.location.Geocoder
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import gov.nasa.gsfc.icesat2.icesat_2.favoritesdb.FavoritesEntry
@@ -19,6 +21,7 @@ class FavoritesAdapter(private val context: Context, private val allFavorites: L
     inner class FavoritesHolder(private val view: View) : RecyclerView.ViewHolder(view){
         var textViewDateTime: TextView = view.findViewById(R.id.textViewDateTime)
         var textViewLatLng: TextView = view.findViewById(R.id.textViewLatLng)
+        var locationListLinearLayout: LinearLayout = view.findViewById(R.id.locationListLinearLayout)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoritesHolder {
@@ -34,6 +37,11 @@ class FavoritesAdapter(private val context: Context, private val allFavorites: L
         holder.textViewLatLng.text = context.getString(
             R.string.geoLatLng, favorite.geocodedLocation,
             favorite.lat.toString(), 0x00B0.toChar(), favorite.lng.toString(), 0x00B0.toChar())
+
+        holder.locationListLinearLayout.setOnClickListener {
+            Log.d(TAG, "onClick at Position ${holder.adapterPosition}")
+            Log.d(TAG, "data of point is ${allFavorites[holder.adapterPosition].dateString}")
+        }
 
     }
 
