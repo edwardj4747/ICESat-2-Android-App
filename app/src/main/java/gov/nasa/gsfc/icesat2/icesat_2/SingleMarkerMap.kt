738c979054819b9a6ee489e691f60115813b3547
Edward <edward.jahoda@gmail.com>
2020-06-20 18:24:28 -0400

    Pass nav args to SingleMarker map and plot/zoom to point

--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/SingleMarkerMap.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/SingleMarkerMap.kt
@@ -5,34 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SingleMarkerMap.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SingleMarkerMap : Fragment(), OnMapReadyCallback {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private lateinit var mMap: GoogleMap
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val args by navArgs<SingleMarkerMapArgs>()
+    private lateinit var title: String
+    private var lat = 0.0
+    private var long = 0.0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,6 +32,10 @@ class SingleMarkerMap : Fragment(), OnMapReadyCallback {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        lat = args.lat.toDouble()
+        long = args.long.toDouble()
+        title = args.title
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = childFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -54,27 +45,11 @@ class SingleMarkerMap : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(p0: GoogleMap) {
         mMap = p0
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SingleMarkerMap.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SingleMarkerMap().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val markerPosition = LatLng(lat, long)
+        val marker = mMap.addMarker(MarkerOptions().position(markerPosition).title(title))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(markerPosition, 15F))
+        marker.showInfoWindow()
     }
-
 
 }
