3bb4dce0435ae30d3a07ea7949c681cd191fcb39
Edward <edward.jahoda@gmail.com>
2020-06-23 10:28:28 -0400

    Renamed Geocoding to IGeocoding

--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/IGeocoding.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/IGeocoding.kt
@@ -19,7 +19,6 @@ interface IGeocoding {
         //returns {locality, 'state', country}
         var locationString = ""
 
-
         if (!address[0].locality.isNullOrEmpty()) {
             locationString += "${address[0].locality}, "
         }
@@ -38,37 +37,3 @@ interface IGeocoding {
     }
 }
 
-class Geocoding() {
-    companion object {
-        fun getAddress(context: Context, lat: Double, long: Double) : String {
-            val geocoder = Geocoder(context)
-            val address = geocoder.getFromLocation(lat, long, 1)
-            return address[0].getAddressLine(0)
-        }
-
-        fun getGeographicInfo(geocoder: Geocoder, lat: Double, long: Double): String {
-            val address = geocoder.getFromLocation(lat, long, 1)
-
-            //returns {locality, 'state', country}
-            var locationString = ""
-
-
-            if (!address[0].locality.isNullOrEmpty()) {
-                locationString += "${address[0].locality}, "
-            }
-            if (!address[0].adminArea.isNullOrEmpty()) {
-                locationString += "${address[0].adminArea}, "
-            }
-            if (!address[0].countryName.isNullOrEmpty()) {
-                locationString += "${address[0].countryName}; "
-            }
-
-            if (locationString == "") {
-                locationString = "Unknown Location, "
-            }
-
-            return locationString
-        }
-    }
-
-}
