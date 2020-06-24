e6c6a521fbe7e114bb6aac8585511d0f6c68edd8
Edward <edward.jahoda@gmail.com>
2020-06-24 13:07:55 -0400

    created satellite tracking fragment

---
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/SatelliteTrackingFragment.kt
@@ -0,0 +1,39 @@
+package gov.nasa.gsfc.icesat2.icesat_2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class SatelliteTrackingFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_satellite_tracking, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+
+    }
+}
