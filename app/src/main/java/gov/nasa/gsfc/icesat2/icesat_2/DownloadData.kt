0103a4199f93a4d33906b510db66064a8ce4a89e
Edward <edward.jahoda@gmail.com>
2020-07-06 13:47:34 -0400

    added 4s timeout

--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/DownloadData.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/DownloadData.kt
@@ -13,7 +13,7 @@ private const val TAG = "DownloadData"
 private const val DATE_ALREADY_PASSED = "DATE_ALREADY_PASSED"
 //Todo:test this for dates way far away in the future
 private const val DATE_DIVISOR = 1000
-private const val JOB_TIMEOUT = 10L
+private const val JOB_TIMEOUT = 4000L
 
 class DownloadData(private val url: URL, context: Context) {
 
