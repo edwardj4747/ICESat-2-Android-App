9347874f620549c288dd2f50a3710b74fe21f6fc
Edward <edward.jahoda@gmail.com>
2020-07-20 13:04:00 -0400

    make direct changes to db code cleanup

--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/ui/favorites/FavoritesFragment.kt
@@ -23,12 +23,7 @@ private const val TAG = "FavoritesFragment"
 class FavoritesFragment : Fragment(), ILaunchSingleMarkerMap {
 
     private lateinit var favoritesViewModel: FavoritesViewModel
-    //private lateinit var favoritesList: List<FavoritesEntry>
     private lateinit var favoritesList: ArrayList<FavoritesEntry>
-    private var swipeListenerAttached = false
-    private var recyclerViewInitialized = false
-    private lateinit var localFavoritesList: ArrayList<FavoritesEntry>
-    private val localDeletedFavorites = ArrayList<Long>() //to keep track of which favorites were deleted and used to update database on stop
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -39,11 +34,6 @@ class FavoritesFragment : Fragment(), ILaunchSingleMarkerMap {
                 ViewModelProviders.of(this).get(FavoritesViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_favorite, container, false)
 
-      /*  favoritesViewModel.getAllFavorites().observe(viewLifecycleOwner, Observer {
-            Log.d(TAG, "allFavorites size is ${it.size}: $it")
-            favoritesList = it
-            initializeRecyclerView()
-        })*/
 
         favoritesViewModel.getAllFavorites().observe(viewLifecycleOwner, Observer {
             Log.d(TAG, "OBSERVED. size is ${it.size}")
@@ -82,78 +72,10 @@ class FavoritesFragment : Fragment(), ILaunchSingleMarkerMap {
                                 displayNoFavoritesTextIfNecessary()
                             }
                             .show()
-
-                        /*Log.d(TAG, "pos is $pos")
-                        val deletedFavorite = favoritesList.removeAt(pos)
-                        localDeletedFavorites.add(deletedFavorite.dateObjectTime)
-
-                        adapter.notifyItemRemoved(pos)
-                        displayNoFavoritesTextIfNecessary()
-                        Log.d(TAG, "After removing, new size is ${favoritesList.size}")
-                        Snackbar.make(snackbarCoordinator, R.string.itemDeleted, Snackbar.LENGTH_LONG)
-                            .setAction(R.string.undo) {
-                                favoritesList.add(pos, deletedFavorite)
-                                localDeletedFavorites.removeAt(localDeletedFavorites.size - 1)
-                                adapter.notifyItemInserted(pos)
-                                displayNoFavoritesTextIfNecessary()
-                            }
-                            .show()*/
                     } catch (e: Exception) { Log.d(TAG, "error in onSwiped ${e.message}") }
                 }
             }).attachToRecyclerView(favoriteRecyclerView)
     }
-
-   /*private fun initializeRecyclerView() {
-       recyclerViewInitialized = true
-       //todo: this is kind of inefficient
-       localFavoritesList = ArrayList(favoritesList)
-
-       displayNoFavoritesTextIfNecessary()
-
-       val adapter = FavoritesAdapter(requireContext(), localFavoritesList)
-       adapter.setListener(this)
-       favoriteRecyclerView.adapter = adapter
-       favoriteRecyclerView.layoutManager = LinearLayoutManager(requireContext())
-
-
-           *//*Log.d(TAG, "Attaching swipe listener")
-           swipeListenerAttached = true*//*
-           //delete swiping
-           ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
-               override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
-                   return false
-               }
-
-               override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                   try {
-                       val deletedFavoritePosition = viewHolder.adapterPosition
-                       val deletedFavorite = localFavoritesList[deletedFavoritePosition]
-                       //val deletedFavorite = adapter.getFavoriteAt(viewHolder.adapterPosition)
-                       //favoritesViewModel.delete(deletedFavorite.dateObjectTime)
-                       //Snackbar.make(this@FavoritesFragment.requireView(), R.string.itemDeleted, Snackbar.LENGTH_LONG)
-                       Snackbar.make(snackbarCoordinator, R.string.itemDeleted, Snackbar.LENGTH_LONG)
-                           .setAction(R.string.undo) {
-                               //favoritesViewModel.insert(deletedFavorite)
-                               localDeletedFavorites.remove(deletedFavorite.dateObjectTime)
-                               localFavoritesList.add(deletedFavoritePosition, deletedFavorite)
-                               adapter.notifyItemInserted(deletedFavoritePosition)
-                               displayNoFavoritesTextIfNecessary()
-                           }
-                           .show()
-                       localDeletedFavorites.add(deletedFavorite.dateObjectTime)
-                       localFavoritesList.removeAt(viewHolder.adapterPosition)
-                       adapter.notifyItemRemoved(viewHolder.adapterPosition)
-                       displayNoFavoritesTextIfNecessary()
-                   } catch (e: Exception) {
-                       Log.d(TAG, "error in onswiped ${e.message}")
-                       Log.d(TAG, "${e.stackTrace}")
-                   }
-
-               }
-           }).attachToRecyclerView(favoriteRecyclerView)
-
-
-   }*/
 
     private fun displayNoFavoritesTextIfNecessary() {
         if (favoritesList.isEmpty()) {
@@ -186,16 +108,6 @@ class FavoritesFragment : Fragment(), ILaunchSingleMarkerMap {
             }
             ?.setNegativeButton(negative) {dialog, which ->  }
         alertBuilder.show()
-    }
-
-
-    //delete items from the database
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "number of favorites to remove is ${localDeletedFavorites.size}")
-        for (i in 0 until localDeletedFavorites.size) {
-            favoritesViewModel.delete(localDeletedFavorites[i])
-        }
     }
 
     //navigates from favorites adapter to single marker map
