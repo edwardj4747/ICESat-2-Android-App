e1ff10389a0a5596b0dca82ded9cca0662d0a9c4
Edward <edward.jahoda@gmail.com>
2020-07-28 14:57:50 -0400

    past points list

--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/DownloadData.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/DownloadData.kt
@@ -12,6 +12,7 @@ import kotlin.collections.ArrayList
 
 private const val TAG = "DownloadData"
 private const val DATE_ALREADY_PASSED = "DATE_ALREADY_PASSED"
+private const val DATE_IN_FUTURE = "DATE_IN_FUTURE"
 //Todo:test this for dates way far away in the future
 private const val DATE_DIVISOR = 1000
 private const val JOB_TIMEOUT = 4000L
@@ -67,6 +68,7 @@ class DownloadData(private val url: URL, context: Context) {
                 val state = jsonObject.getString("state")
                 if (state == "true") {
                     val pointsArrayList = ArrayList<Point>()
+                    val pastPointsArrayList = ArrayList<Point>()
                     val queryResult = jsonObject.getJSONArray("result")
                     for (i in 0 until queryResult.length()) {
                         val individualPoint = queryResult.getJSONObject(i)
@@ -83,13 +85,16 @@ class DownloadData(private val url: URL, context: Context) {
                         }
 
                         //Wait until conversion is completed. Add the point only if it is in future
-                        if (convertedDateTime.await()[0] != DATE_ALREADY_PASSED) {
-                            //val newPoint = Point(date, time, lon, lat, convertedDateTime.await()[0] as String, convertedDateTime.await()[1] as Date)
-                            //TODO: build in a second constructor in case this one fails
+                        if (convertedDateTime.await()[8] != DATE_ALREADY_PASSED) {
                             val newPoint = Point(convertedDateTime.await()[0] as String, convertedDateTime.await()[1] as String,
                                 convertedDateTime.await()[2] as String, convertedDateTime.await()[3] as String, convertedDateTime.await()[4] as String,
                             convertedDateTime.await()[5] as String, convertedDateTime.await()[6] as String, lon, lat, convertedDateTime.await()[7] as Date)
                             pointsArrayList.add(newPoint)
+                        } else {
+                            val oldPoint = Point(convertedDateTime.await()[0] as String, convertedDateTime.await()[1] as String,
+                                convertedDateTime.await()[2] as String, convertedDateTime.await()[3] as String, convertedDateTime.await()[4] as String,
+                                convertedDateTime.await()[5] as String, convertedDateTime.await()[6] as String, lon, lat, convertedDateTime.await()[7] as Date)
+                            pastPointsArrayList.add(oldPoint)
                         }
                     }
                     val mainActivityViewModel = MainActivity.getMainViewModel()
@@ -105,6 +110,12 @@ class DownloadData(private val url: URL, context: Context) {
 
                         Log.d(TAG, "AFter sorting $pointsArrayList")
 
+                        //sort the past points
+                        val sortPastPointArrayUnit: Deferred<Unit> = async {
+                            Collections.sort(pastPointsArrayList, comparator)
+                        }
+                        sortPastPointArrayUnit.await()
+
                        /* val allPointChains: Deferred<ArrayList<ArrayList<Point>>> = async {
                             splitPointsByDate(pointsArrayList)
                         }*/
@@ -115,6 +126,9 @@ class DownloadData(private val url: URL, context: Context) {
                             mainActivityViewModel.allPointsList.postValue(pointsArrayList)
                             //mainActivityViewModel.allPointsChain.postValue(allPointChains.await())
                             resultsFound = true
+
+                            //post the oldPoints too
+                            mainActivityViewModel.pastPointsList.postValue(pastPointsArrayList)
                         }
                     } else {
                         //if we don't find any results post an empty list. Removes carryovers from displaying in searches that have no result
@@ -147,16 +161,16 @@ class DownloadData(private val url: URL, context: Context) {
      * @param dateString downloaded date of form 22-7-2020 (ie: July 22, 2020)
      * @param timeString time of the satellite flyover in UTC
      * If the date has already passed, will return arrayOf(DATE_ALREADY_PASSED, null) otherwise
-     * @return arrayOf(convertedDateString, dayOfWeek, date, year, timeString, AM/PM, timezone, dateTimeToConvert)
+     * @return arrayOf(convertedDateString, dayOfWeek, date, year, timeString, AM/PM, timezone, dateTimeToConvert, inPast/inFuture)
      */
     private fun convertDateTime(dateString: String, timeString: String): Array<Any?> {
         val inputFormat = SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault())
         inputFormat.timeZone = TimeZone.getTimeZone("UTC")
         val inputDate = "$dateString $timeString"
         val dateTimeToConvert = inputFormat.parse(inputDate)
-        if (dateTimeToConvert.before(currentTime)) {
+        /*if (dateTimeToConvert.before(currentTime)) {
             return arrayOf(DATE_ALREADY_PASSED, null)
-        }
+        }*/
         val outputFormat = SimpleDateFormat("EEE, MMM d, yyyy, hh:mm:ss, aaa, z", Locale.getDefault())
         outputFormat.timeZone = TimeZone.getDefault()
         val convertedDateString = outputFormat.format(dateTimeToConvert)
@@ -164,9 +178,15 @@ class DownloadData(private val url: URL, context: Context) {
         //split up the date string
         val convertedDateStringSplit = convertedDateString.split(",")
 
+        val alreadyPassed = if (dateTimeToConvert.before(currentTime)) {
+            DATE_ALREADY_PASSED
+        } else {
+            DATE_IN_FUTURE
+        }
+
         return arrayOf(convertedDateString, convertedDateStringSplit[0], convertedDateStringSplit[1].trimStart(),
             convertedDateStringSplit[2].trimStart(), convertedDateStringSplit[3].trimStart(), convertedDateStringSplit[4].trimStart(),
-            convertedDateStringSplit[5].trimStart(), dateTimeToConvert)
+            convertedDateStringSplit[5].trimStart(), dateTimeToConvert, alreadyPassed)
 
         //return arrayOf(convertedDateString, dateTimeToConvert)
     }
