d6fc3ea3ef46766cb08b8827f959e464c97b5f5f
Edward <edward.jahoda@gmail.com>
2020-08-03 14:07:15 -0400

    map popup window show latitude and longitude

--- app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/MapFragment.kt
+++ app/src/main/java/gov/nasa/gsfc/icesat2/icesat_2/MapFragment.kt
@@ -215,7 +215,8 @@ GoogleMap.OnPolylineClickListener {
             //add marker to map and markerList
             val markerAdded = mMap.addMarker(
                 myMarker.position(LatLng(chain[i].latitude, chain[i].longitude))
-                    .title(chain[i].dateString)
+                    //.title(chain[i].dateString)
+                    .title(getString(R.string.latLngDisplayString, chain[i].latitude.toString(), 0x00B0.toChar(), chain[i].longitude.toString(), 0x00B0.toChar()))
             )
             markerAdded.tag = count
             markerList.add(markerAdded)
